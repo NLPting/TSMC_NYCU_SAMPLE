3e74f48612d2b18926d7a7f2f062345f3e689af3
xdavidwu <xdavidwuph@gmail.com>
2022-06-04 22:39:08 +0800

    test: remove html_parser
    it was refactored out
    
    in original sample code, it is just plain BeautifulSoup, without any our
    logic
    
    testing just external libraries is not our concern

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -19,14 +19,6 @@ def test_get_source():
     response = crawler.get_source(target_url)
     assert response.status_code == 200
 
-def test_html_parser():
-    crawler = crawler_sample.GoogleCrawler()
-    target_url = 'https://www.reuters.com/technology/exclusive-ukraine-halts-half-worlds-neon-output-chips-clouding-outlook-2022-03-11/'
-    response = crawler.get_source(target_url)
-    soup = crawler.html_parser(response.text)
-    results = soup.findAll("div")
-    assert len(results) > 0
-
 def test_scrape_google():
     query = 'https://www.google.com/search?q='+"TSMC Ingas"
     crawler = crawler_sample.GoogleCrawler()
